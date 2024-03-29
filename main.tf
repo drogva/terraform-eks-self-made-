b4bed304974e60cae52e1db3fb2b1dc6695bfb3f
drogva <156391770+drogva@users.noreply.github.com>
2024-03-29 15:49:30 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -201,7 +201,7 @@ module "aws_load_balancer_controller_irsa_role" {
 
 resource "kubernetes_service_account" "aws_load_balancer_controller" {
   metadata {
-    name      = "aws-load-balancer-controller-sa"
+    name      = "aws-load-balancer-controller"
     namespace = "kube-system"
 
     labels = {
