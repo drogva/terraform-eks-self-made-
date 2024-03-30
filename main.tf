b65daba9e09c1b665d0c9b0b58069de704cd06a3
drogva <156391770+drogva@users.noreply.github.com>
2024-03-30 20:50:45 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -91,21 +91,7 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEKSServicePolicy" {
   role       = aws_iam_role.example.name
 }
 
-module "ebs_csi_irsa" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
 
-  role_name = "ebs-csi"
-  attach_ebs_csi_policy = true
-
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
-    }
-  }
-
-
-}
 
 
 
