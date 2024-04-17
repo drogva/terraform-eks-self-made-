cd91da68168c33b19a3afa4dc702edd91da1a2d7
drogva <156391770+drogva@users.noreply.github.com>
2024-04-17 10:55:34 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,8 +15,11 @@ module "eks" {
   }
 
   eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-  }
+  ami_type = "AL2_x86_64"
+  instance_type = "t3.small"
+  update_launch_template_default_version = true
+  iam_role_additional_policies = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"]
+}
 
   eks_managed_node_groups = {
     one = {
